4f6e608631dd1dda8a67572535abe3d3c8eaf042
santhosh-rameshh <154718128+santhosh-rameshh@users.noreply.github.com>
2024-08-24 15:43:50 +0530

    Update views.py

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -13,8 +13,6 @@ from .restapis import get_request, analyze_review_sentiments, post_review
 logger = logging.getLogger(__name__)
 
 
-# Create your views here.
-
 # Create a `login_request` view to handle sign in request
 @csrf_exempt
 def login_user(request):
