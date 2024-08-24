02e256bf882755f9a782055fc80797cced29422d
santhosh-rameshh <154718128+santhosh-rameshh@users.noreply.github.com>
2024-08-24 15:51:52 +0530

    Update settings.py

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -27,7 +27,8 @@ SECRET_KEY =\
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-link = 'https://santhoshrmca-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai'
+link = 'https://santhoshrmca-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.' +
+'proxy.cognitiveclass.ai'
 ALLOWED_HOSTS = ['localhost', link]
 CSRF_TRUSTED_ORIGINS = [link]
 
@@ -94,7 +95,8 @@ DATABASES = {
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME':
-        'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        'django.contrib.auth.password_validation.' +
+        'UserAttributeSimilarityValidator',
     },
     {
         'NAME':
